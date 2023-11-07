c4b5a5e2405721e87c23979ad00d7c68fa6c8e72
Alfonso Gonzalez <info@wsemantic.com>
2023-11-07 17:50:02 +0100

    Update __manifest__.py nueva version

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '16.0.0.2',
+    'version': '16.0.0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['l10n_es_aeat_sii_oca'],
